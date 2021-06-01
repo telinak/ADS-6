381717e75bb24163f003c07de3518985cdd9fd22
telinak <78906896+telinak@users.noreply.github.com>
2021-06-01 17:20:08 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,24 +3,24 @@
 #define INCLUDE_BST_H_
 template<typename T>
 class BST {
-  public:
-  struct Node {
-    T value;
-    int count;
-    Node* left;
-    Node* right;
-  };
-  private:
-  Node* root;
-  Node* addNode(Node*, T);
-  int depthTree(Node*);
-  int searchNode(Node*, T);
-  public:
-  BST();
-  ~BST();
-  void add(T);
-  int depth();
-  int search(T);
+ public:
+    struct Node {
+        T value;
+        int count;
+        Node* left;
+        Node* right;
+    };
+ private:
+    Node* root;
+    Node* addNode(Node*, T);
+    int depthTree(Node*);
+    int searchNode(Node*, T);
+ public:
+    BST();
+    ~BST();
+    void add(T);
+    int depth();
+    int search(T);
 };
 
 template<typename T>
